4f96130be2b54bd6c577046644793bc00e5fc7df
Ruper Lopez <lopezruper@gmail.com>
2023-11-13 15:38:52 +0100

    unicafe finished

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -24,21 +24,13 @@ const Total = ({ parts }) => {
   return (
     <p>
       {" "}
-      Number of exercises {' '}
+      Number of exercises{" "}
       {parts.reduce((total, current) => total + current.exercises, 0)}
     </p>
   );
 };
 
 const App = () => {
-  // const course = "Half Stack application development";
-  // const part1 = "Fundamentals of React";
-  // const exercises1 = 10;
-  // const part2 = "Using props to pass data";
-  // const exercises2 = 7;
-  // const part3 = "State of a component";
-  // const exercises3 = 14;
-
   const course = {
     name: "Half Stack application development",
     parts: [
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td> {text} </td>
+      <td> {value} </td>
+    </tr>
+  );
+};
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total || 0;
+  const positive = (good / total) * 100 || 0;
+
+  return (
+    <>
+      <h1>Statistics</h1>
+      {total ? (
+        <table>
+          <tbody>
+            <StatisticLine text="Good" value={good} />
+            <StatisticLine text="Neutral" value={neutral} />
+            <StatisticLine text="Bad" value={bad} />
+            <StatisticLine text="All" value={total} />
+            <StatisticLine text="Average" value={average} />
+            <StatisticLine text="Positive" value={`${positive}%`} />
+          </tbody>
+        </table>
+      ) : (
+        <p>No feedback given</p>
+      )}
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <h1> give feedback</h1>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+  );
+};
+
+export default App;
