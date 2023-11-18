86621ee10802bf4381885fd4861dc0e7adfdddde
Ruper Lopez <lopezruper@gmail.com>
2023-11-18 19:05:11 +0100

    add part2-getting-data-from-server

--- practice/2-app/src/App.jsx
+++ practice/2-app/src/App.jsx
@@ -1,41 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes); // guardamos notas en component state
-  // const [notes, setNotes] = useState([]) // asi se inicializa sin notas
+  const [notes, setNotes] = useState([]); // inicializamos con rango vacio porque tenemos database
   const [newNote, setNewNote] = useState("a new note...");
-  //para mostrar solamente notas importantes
   const [showAll, setShowAll] = useState(true);
 
+  // esta funcion es ejectada justo despues de renderizar
+const hook = () => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+}
+
+useEffect(hook, [])
+
   const addNote = (event) => {
-    // argumento es evento que se dispara
-    event.preventDefault(); // hace que la pagina se recarge
-    console.log("button clicked", event.target); // target en este caso es el form
-
-    // una luego de agregar onChange, newNote refleja el current value del input
-
-    // creamos objeto que recibe contenido de newNote
+    event.preventDefault();
+    console.log("button clicked", event.target);
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5, // probabilidad de que la nota sea importante
-      // creamos id basado en el numero de notes que haya
-      // este metodo funciona porque en esta app no se pueden borrar notas
-      id: notes.length + 1, // creamos id basado en el numero de notes que haya
+      important: Math.random() < 0.5, 
+      id: notes.length + 1, 
     };
 
-    // concatenamos nota nueva a las notas
-    // usamos concatenar porque no se pueden mutar estados directamente
     setNotes(notes.concat(noteObject));
-    setNewNote(""); // reseteamos el estado
+    setNewNote(""); 
   };
 
   const handleNoteChange = (event) => {
-    // llamado cada vez que sucede un cambio en input element
-    // no ponemeos event.preventDefault, porque con inputs no hay comportamiento default
-    // con form si los hay
-    console.log(event.target.value); // event ahora corresponde a input
-    setNewNote(event.target.value); // event.target.value es el input value del elemento
+    console.log(event.target.value);
+    setNewNote(event.target.value); 
   };
 
   const notesToShow = showAll
@@ -51,16 +51,13 @@ const App = (props) => {
         </button>
       </div>
       <ul>
-        {/** notesToShow instead notes, muestra solo importantes*/}
         {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
       <form onSubmit={addNote}>
         {" "}
-        {/** event handler */}
         <input value={newNote} onChange={handleNoteChange} />
-        {/** app component maneja el comportamiento de input, por eso agregamos onChange*/}
         <button type="submit">save</button>
       </form>
     </div>
