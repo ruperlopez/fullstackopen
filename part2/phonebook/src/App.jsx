ab94d3f23f37d33efcd0c28a3a7aab6c805be458
Ruper Lopez <lopezruper@gmail.com>
2023-11-21 14:47:47 +0100

    exercise 2.9 - adding filter

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "123" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -13,7 +17,7 @@ const App = () => {
     if (persons.find((person) => person.name === newName)) {
       return alert(`${newName} is already added to phonebook`);
     }
-    setPersons([...persons, { name: newName , number: newNumber}]);
+    setPersons([...persons, { name: newName, number: newNumber }]);
     setNewName("");
     setNewNumber("");
   };
@@ -24,10 +28,21 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase()),
+  );
 
   return (
     <>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input type="search" value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2> add a new </h2>
       <form onSubmit={addPerson}>
         <div>
           name:{" "}
@@ -42,9 +57,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}> {person.name}: {person.number} </div>
-      ))}
+        {filteredPersons.map((person) => (
+        <div key={person.name}> {person.name}: {person.number}</div>
+        ))}
     </>
   );
 };
