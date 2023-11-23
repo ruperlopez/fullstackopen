22375d12ef7c32a70687f863f12648d5da904e95
Ruper Lopez <lopezruper@gmail.com>
2023-11-23 20:34:48 +0100

    app deployed with fly.io

--- practice/3/index.js
+++ practice/3/index.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
 // middleware para imprimir info acerca de los request que mandamos al servidor
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
 // middleware para agarrar request hecho a rutas no existentes
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(cors())
+app.use(cors());
 // el orden de estos middlewares tiene que ser asi
-app.use(express.json()) // json-parser: es un middleware que importamos
-app.use(requestLogger) // inicializamos middle ware creado por nosotros
+app.use(express.json()); // json-parser: es un middleware que importamos
+app.use(requestLogger); // inicializamos middle ware creado por nosotros
 
 let notes = [
   {
@@ -61,46 +61,44 @@ app.get("/api/notes/:id", (request, response) => {
 });
 
 // Deleting resources
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
 
-  response.status(204).end() // si se borra respondemos con 204, tmb podria ser 404
-})
+  response.status(204).end(); // si se borra respondemos con 204, tmb podria ser 404
+});
 
 // helper function for receiving data
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
 // receiving data
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
   const note = {
     content: body.content, // data obligatoria
     important: body.important || false, // si no esta esta data va falso
     id: generateId(),
-  }
+  };
 
-  notes = notes.concat(note)
+  notes = notes.concat(note);
 
-  response.json(note)
-})
+  response.json(note);
+});
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 // listening to port
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
